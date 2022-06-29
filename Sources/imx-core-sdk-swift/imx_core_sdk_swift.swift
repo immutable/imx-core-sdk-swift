678535515c9e101606e5792d7a1fba3a7f6114b3
Cassius Pacheco <cassius.pacheco@immutable.com>
2022-06-29 11:01:03 +1000

    chore: initial commit

---
+++ Sources/imx-core-sdk-swift/imx_core_sdk_swift.swift
@@ -0,0 +1,6 @@
+public struct imx_core_sdk_swift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/imx-core-sdk-swiftTests/imx_core_sdk_swiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import imx_core_sdk_swift
+
+final class imx_core_sdk_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(imx_core_sdk_swift().text, "Hello, World!")
+    }
+}
