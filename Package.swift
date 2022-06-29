8de860cfec0ad0c41bf9b42d265ae1d38953d356
Cassius Pacheco <cassius.pacheco@immutable.com>
2022-06-29 13:49:38 +1000

    ci: setup SwiftLint in the package
    Packages don't have build phase scripts, which requires a bit of a
    workaround to hook in SwiftLint's binary as a plugin in order to show
    the appropriate errors/warnings on Xcode.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// swiftlint:disable line_length
 
 import PackageDescription
 
@@ -7,23 +8,34 @@ let package = Package(
     name: "IMXCoreSDK",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
             name: "IMXCoreSDK",
             targets: ["IMXCoreSDK"]
-        ),
+        )
     ],
     dependencies: [],
     targets: [
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/juozasvalancius/SwiftLint/releases/download/spm-accommodation/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "cdc36c26225fba80efc3ac2e67c2e3c3f54937145869ea5dbcaa234e57fc3724"
+        ),
+        .plugin(
+            name: "SwiftLintXcode",
+            capability: .buildTool(),
+            dependencies: ["SwiftLintBinary"]
+        ),
         .target(
             name: "IMXCoreSDK",
-            dependencies: []
+            dependencies: [],
+            plugins: ["SwiftLintXcode"]
         ),
         .testTarget(
             name: "IMXCoreSDKTests",
             dependencies: ["IMXCoreSDK"]
-        ),
+        )
     ]
 )
