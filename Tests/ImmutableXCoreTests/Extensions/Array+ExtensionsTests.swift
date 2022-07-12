fb1663d223cae415b5cd8ae281fca0665067a30d
Cassius Pacheco <cassius.pacheco@immutable.com>
2022-07-12 18:08:01 +1000

    refactor: expose asStarkKey property in PublicKey
    The public stark key requires some sanitisation before being used. The
    code has been updated to reflect that specification.

---
+++ Tests/ImmutableXCoreTests/Extensions/Array+ExtensionsTests.swift
@@ -0,0 +1,15 @@
+import BigInt
+@testable import ImmutableXCore
+import XCTest
+
+final class ArrayExtensionsTests: XCTestCase {
+    func testAsHexString() throws {
+        let hex = "3035919acd61e97b3ecdc75ff8beed8d1803f7ea3cad2937926ae59cc3f8070d4"
+        XCTAssertEqual(
+            hex.sanitizeBytes().hexaToBytes.asHexString(byteLength: 32), "0x035919acd61e97b3ecdc75ff8beed8d1803f7ea3cad2937926ae59cc3f8070d4"
+        )
+        XCTAssertEqual(
+            hex.sanitizeBytes(byteSize: 2).hexaToBytes.asHexString(byteLength: 64), "0x0" + hex
+        )
+    }
+}
--- Tests/ImmutableXCoreTests/Extensions/String+ExtensionsTests.swift
+++ Tests/ImmutableXCoreTests/Extensions/String+ExtensionsTests.swift
@@ -18,4 +18,16 @@ final class StringExtensionsTests: XCTestCase {
         XCTAssertEqual("2b6f".hexToData(), Data([43, 111]))
         XCTAssertEqual("0x2b6f".hexToData(), Data([43, 111]))
     }
+
+    func testPadLeft() throws {
+        XCTAssertEqual("1".padLeft(length: 8), "00000001")
+        XCTAssertEqual("".padLeft(length: 8, padding: "1"), "11111111")
+    }
+
+    func testPadString() throws {
+        XCTAssertEqual("1".padString(length: 8, left: false), "10000000")
+        XCTAssertEqual("1".padString(length: 8, left: true), "00000001")
+        XCTAssertEqual("".padString(length: 8, left: false, padding: "1"), "11111111")
+        XCTAssertEqual("".padString(length: 8, left: true, padding: "1"), "11111111")
+    }
 }
