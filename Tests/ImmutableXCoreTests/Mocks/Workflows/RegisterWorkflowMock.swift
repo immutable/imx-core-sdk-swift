0be32f4ad9b6d36de845e9f02a804461cd0d691c
Cassius Pacheco <cassius.pacheco@immutable.com>
2022-07-27 14:53:18 +1000

    feat(workflow): expose registerOffchain workflow

---
+++ Tests/ImmutableXCoreTests/Mocks/Workflows/RegisterWorkflowMock.swift
@@ -0,0 +1,31 @@
+import Foundation
+@testable import ImmutableXCore
+
+class RegisterWorkflowCompanion {
+    var throwableError: Error?
+    var callsCount = 0
+    var returnValue: Bool = true
+}
+
+class RegisterWorkflowMock: RegisterWorkflow {
+    static var companion: RegisterWorkflowCompanion!
+
+    static func mock(_ companion: RegisterWorkflowCompanion) {
+        self.companion = companion
+    }
+
+    static func resetMock() {
+        companion = nil
+    }
+
+    override class func registerOffchain(signer: Signer, starkSigner: StarkSigner, usersAPI: UsersAPI.Type = UsersAPI.self) async throws -> Bool {
+        let companion = companion!
+        companion.callsCount += 1
+
+        if let error = companion.throwableError {
+            throw error
+        }
+
+        return companion.returnValue
+    }
+}
