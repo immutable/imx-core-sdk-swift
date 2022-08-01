b3c9eb18cf6ffe7e0a37ecf9eb23063536ce80c6
Cassius Pacheco <cassius.pacheco@immutable.com>
2022-08-01 16:21:33 +1000

    feat(moonpay): implement MoonpayAPI
    Returns the Moonpay URL for the given `request` params

---
+++ Tests/ImmutableXCoreTests/Mocks/RequestBuilderFactoryMock.swift
@@ -0,0 +1,39 @@
+import Foundation
+@testable import ImmutableXCore
+
+class RequestBuilderFactoryMock: RequestBuilderFactory {
+    static var returnValue: Result<Response<Any>, ErrorResponse>!
+
+    func mock<T>(_ result: Result<Response<T>, ErrorResponse>) {
+        switch result {
+        case let .success(response):
+            RequestBuilderFactoryMock.returnValue = .success(Response(response: HTTPURLResponse(), body: response.body))
+        case let .failure(error):
+            RequestBuilderFactoryMock.returnValue = .failure(error)
+        }
+    }
+
+    func getNonDecodableBuilder<T>() -> RequestBuilder<T>.Type {
+        RequestBuilderMock.self
+    }
+
+    func getBuilder<T>() -> RequestBuilder<T>.Type where T: Decodable {
+        RequestBuilderMock.self
+    }
+}
+
+class RequestBuilderMock<T>: RequestBuilder<T> {
+    @discardableResult
+    override open func execute(_ apiResponseQueue: DispatchQueue = OpenAPIClientAPI.apiResponseQueue, _ completion: @escaping (_ result: Swift.Result<Response<T>, ErrorResponse>) -> Void) -> RequestTask {
+        switch RequestBuilderFactoryMock.returnValue {
+        case let .success(response):
+            completion(.success(Response(response: HTTPURLResponse(), body: response.body as! T)))
+        case let .failure(error):
+            completion(.failure(error))
+        case .none:
+            fatalError("Return type not set")
+        }
+
+        return RequestTask()
+    }
+}
