ab3c6df74c9d54b0db82641a277a38067d1c069f
Cassius Pacheco <cassius.pacheco@immutable.com>
2022-08-05 17:37:21 +1000

    docs: remove docs folder
    The issue of hosting the static files in our website has been fixed.
    The Github Page has been taken down in favour of our website's docs
    https://docs.x.immutable.com/sdk-docs/core-sdk-swift/overview
    
    The script to generate the static files has been updated to work with
    our website and versioning.
    
    Docs folder and .doccarchive file have been added to gitignore not to
    be committed by accident.

--- generate-static-docs.sh
+++ generate-static-docs.sh
@@ -1,1 +1,26 @@
-$(xcrun --find docc) process-archive transform-for-static-hosting ImmutableXCore.doccarchive --output-path docs --hosting-base-path /imx-core-sdk-swift
+#!/bin/bash
+
+# Use this script to generate the static files to be hosted on https://docs.x.immutable.com/sdk-docs/core-sdk-swift/overview
+while getopts "v:" OPTION; do
+  case "$OPTION" in
+    v)
+      VERSION=$OPTARG
+      echo "Generating docs for version: $VERSION";;
+    \?)
+      echo "Error: missing version. e.g. -v 1-0-0"
+      exit;;
+    esac
+done
+
+if [[ -z "$VERSION" ]]; then
+    echo "Must provide -v. e.g. -v 1-0-0" 1>&2
+    exit 1
+fi
+
+if [[ "$VERSION" == *"."* ]]; then
+  echo "Version must use dashes instead of periods. e.g. -v 1-0-0" 1>&2
+    exit 1
+fi
+
+$(xcrun --find docc) process-archive transform-for-static-hosting ImmutableXCore.doccarchive --output-path docs --hosting-base-path /sdk-references/core-sdk-swift/$VERSION
+echo "Generated under /docs using hosting-base-path: /sdk-references/core-sdk-swift/$VERSION"
